5df7ac09987adb19b8b9983a14bf390fb0a62d99
Nazarewicz <talibowie@gmail.com>
2014-07-16 11:38:51 +0200

    1136

---
+++ Dziennik/Controllers/UczenController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppDziennik.Models;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+
+namespace AppDziennik.Controllers
+{
+    public class UczenController : Controller
+    {
+        //
+        // GET: /Uczen/
+
+        public ActionResult Index()
+        {
+            var db_u = new UczenDataContext();
+            return View("Index", db_u.Uczniowie.ToList());
+        }
+
+        //
+        // GET: /Uczen/Details/5
+
+        public ActionResult Details(int id_u)
+        {
+            var db_u = new UczenDataContext();
+            var u = db_u.Uczniowie.Find(id_u);
+            return View("Details", u);
+        }
+
+      public ActionResult PartialDetails(int id_u)
+      {
+          var db_o = new DBDziennik();
+          return PartialView("PartialDetails", db_o.Oceny.Where(m => m.Id_u == id_u).ToList());
+      }
+
+        //
+        // GET: /Uczen/Create
+
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        //
+        // POST: /Uczen/Create
+
+        [HttpPost]
+        public ActionResult Create(Uczen U)
+        {
+            var db_u = new UczenDataContext();
+            if (ModelState.IsValid)
+            {
+                db_u.Uczniowie.Add(U);
+                db_u.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            {
+                return View("Create", U);
+            }
+        }
+
+        //
+        // GET: /Uczen/Edit/5
+
+        public ActionResult Edit(int id_u)
+        {
+            var db_u = new UczenDataContext();
+            return View("Edit", db_u.Uczniowie.Find(id_u));
+        }
+
+        //
+        // POST: /Uczen/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Uczen U)
+        {
+            if (ModelState.IsValid)
+            {
+                var db_u = new UczenDataContext();
+                db_u.Uczniowie.AddOrUpdate(U);
+                db_u.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View("Edit", U);
+        }
+
+        //
+        // GET: /Uczen/Delete/5
+
+        public ActionResult Delete(int id_u)
+        {
+            var db_u = new UczenDataContext();
+            var u = db_u.Uczniowie.Find(id_u);
+            return View("Delete", u);
+        }
+
+        //
+        // POST: /Uczen/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id_u, bool form=true)
+        {
+            var db_u = new UczenDataContext();
+            Uczen U = db_u.Uczniowie.Find(id_u);
+            if (ModelState.IsValid)
+            {
+                db_u.Uczniowie.Remove(U);
+                db_u.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("Delete", U);
+
+        }
+    }
+}
---
+++ Dziennik/Models/Dziennik.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppDziennik.Models
+{
+   // public class Dziennik
+   // {
+        public class Uczen
+        {
+            [Key]
+            public int Id_u { set; get; }
+            public string Imie { set; get; }
+            public string Nazwisko { set; get; }
+            public int Wiek { set; get; }
+            public int Klasa { set; get; }
+            public ICollection<Ocena> Oceny { set; get; }
+
+        }
+        public class UczenDataContext : DbContext
+        {
+            public DbSet<Uczen> Uczniowie { set; get; }
+        }
+
+        public class Ocena
+        {
+            [Key]
+            public int Id_o { set; get; }
+            public int Id_u { set; get; }
+            public int Wartosc { set; get; }
+            public Uczen Uczen { set; get; }
+        }
+
+        public class OcenaDataContext : DbContext
+        {
+            public DbSet<Ocena> Oceny { set; get; }
+        }
+
+        public class DBDziennik : DbContext
+        {
+            public DbSet<Uczen> Uczniowie { set; get; }
+            public DbSet<Ocena> Oceny { set; get; }
+        }
+
+   // }
+}
